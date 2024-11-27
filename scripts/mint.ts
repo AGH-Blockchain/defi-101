97be5a27d5faf7002042662f9fdc1cc3200515bb
Mateusz Zając <matzayonc@gmail.com>
2024-11-27 23:54:25 +0100

    added whitelist with names

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -5,22 +5,24 @@ import { getProgram, mintA, mintB } from "./common.ts";
 const program = getProgram();
 
 const accounts = [
-  // // Mateusz Zając
-  // "8bASXozSVHXQWUtRW8uCyx1U3YGDmzKZ37KyGkcJ5dfP",
-  // // Jakub Pośpiech
-  // "BToGfrd3GvY1rJWtqQxFHChoEed1YMiJBqp2sCScgcDv",
-  // // Adam Frydel
-  // "9E6wSeh2orQxij19z1fPZ2eMCE19a4kkiBzsfLrdFKze",
-  // // Kacper Kozera
-  // "7X2PxXwdFgdS7A2rrGrBzvRVA61zgTKbBnHb7aqDVJv7",
-  // // Krzysiek Kaleta
-  // "E2HTs9ZU95bRvxo1Ng2qxPgDzjx3H9yP6i7MUxqpbbzK",
-  // // Katarzyna Bęben
-  // "6F1wQvLqDUM4DFBpRLFFtuoMe5B5scfwHrw4TmgTRGhf",
-  // // Kamil Szulc
-  // "Fa9PAQ1CG5DhvM7UuLfe6K7Yf69ERPsyUh4xEP61K4SX",
-  // // Hipacy Pociej
-  // "FBgY75xFAuBUqg9YqxDD7YkpJfob8rg7b5AhHxV3qPh4",
+  // Mateusz
+  "8bASXozSVHXQWUtRW8uCyx1U3YGDmzKZ37KyGkcJ5dfP",
+  // Jakub
+  "BToGfrd3GvY1rJWtqQxFHChoEed1YMiJBqp2sCScgcDv",
+  // Adam
+  "9E6wSeh2orQxij19z1fPZ2eMCE19a4kkiBzsfLrdFKze",
+  // Kacper
+  "7X2PxXwdFgdS7A2rrGrBzvRVA61zgTKbBnHb7aqDVJv7",
+  // Krzysiek
+  "E2HTs9ZU95bRvxo1Ng2qxPgDzjx3H9yP6i7MUxqpbbzK",
+  // Katarzyna
+  "6F1wQvLqDUM4DFBpRLFFtuoMe5B5scfwHrw4TmgTRGhf",
+  // Kamil
+  "Fa9PAQ1CG5DhvM7UuLfe6K7Yf69ERPsyUh4xEP61K4SX",
+  // Hipacy
+  "FBgY75xFAuBUqg9YqxDD7YkpJfob8rg7b5AhHxV3qPh4",
+  // Oskar
+  "77kogriREbjYqWxBFhsPdPdaTt3dM9Hx96TU9dvrMxMG",
 ];
 
 const amount = 1e9;
